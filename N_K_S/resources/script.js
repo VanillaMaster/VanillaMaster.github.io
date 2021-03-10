adb1caefb0310d4999d01dc4399b6a64a2bba733
VanillaMaster <36816539+VanillaMaster@users.noreply.github.com>
2021-03-10 17:43:44 +0500

    Add files via upload

--- N_K_S/resources/script.js
+++ N_K_S/resources/script.js
@@ -1,14 +1,14 @@
 document.getElementById('app').addEventListener("click", function() {
   document.getElementById("checkBoxMail").checked = false;
-  document.getElementById("checkBoxMail2").checked = false;
+  document.getElementById("checkBoxShowMore").checked = false;
 });
 
-document.getElementsByClassName('clickableList')[0].addEventListener("click", function() {
+document.getElementById('mailButton').addEventListener("click", function() {
   event.stopPropagation();
-  document.getElementById("checkBoxMail2").checked = false;
+  document.getElementById("checkBoxShowMore").checked = false;
 });
 
-document.getElementsByClassName('clickableList')[1].addEventListener("click", function() {
+document.getElementById('showMoreButton').addEventListener("click", function() {
   event.stopPropagation();
   document.getElementById("checkBoxMail").checked = false;
 });
