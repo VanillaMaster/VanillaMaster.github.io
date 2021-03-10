515fbca6ce669a0afecef2ee9e199a7ac13eb34a
VanillaMaster <36816539+VanillaMaster@users.noreply.github.com>
2021-03-10 19:01:07 +0500

    Add files via upload

--- N_K_S/resources/script.js
+++ N_K_S/resources/script.js
@@ -20,6 +20,18 @@ for (let item of elements) {
     if(!item.classList.contains("active")){
       removeActive();
       item.classList.add("active");
+
+      let cards = document.getElementsByClassName("card")
+      for (let card of cards){
+        card.style.display = "none";
+      }
+
+      console.log(item.innerText.replace(/ /g,""));
+      
+      cards = document.getElementsByClassName(item.innerText.replace(/ /g,""))
+      for (let card of cards){
+        card.style.display = "block";
+      }
     }
   });
 }
