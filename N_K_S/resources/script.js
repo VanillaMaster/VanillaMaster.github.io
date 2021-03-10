978ecb56162e2e78e087bc14346e618f11bccf49
VanillaMaster <36816539+VanillaMaster@users.noreply.github.com>
2021-03-10 18:45:45 +0500

    Add files via upload

--- N_K_S/resources/script.js
+++ N_K_S/resources/script.js
@@ -12,3 +12,22 @@ document.getElementById('showMoreButton').addEventListener("click", function() {
   event.stopPropagation();
   document.getElementById("checkBoxMail").checked = false;
 });
+
+let elements = document.getElementById('subNavigation').getElementsByClassName("subNavigationButton")
+for (let item of elements) {
+  item.addEventListener("click", function() {
+    event.preventDefault()
+    if(!item.classList.contains("active")){
+      removeActive();
+      item.classList.add("active");
+    }
+  });
+}
+
+
+function removeActive() {
+  let elements = document.getElementById('subNavigation').getElementsByClassName("subNavigationButton");
+  for (let item of elements) {
+    item.classList.remove("active");
+  }
+}
