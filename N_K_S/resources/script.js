219ab4bbe38f75e1ee578b72f901596d8e543588
VanillaMaster <36816539+VanillaMaster@users.noreply.github.com>
2021-03-19 19:50:47 +0500

    Add files via upload

--- N_K_S/resources/script.js
+++ N_K_S/resources/script.js
@@ -2,23 +2,23 @@ let elements = document.getElementById('subNavigation').getElementsByClassName("
 for (let item of elements) {
   item.addEventListener("click", function() {
     event.preventDefault()
-    if(!item.classList.contains("active")){
+    if(!this.classList.contains("active")){
 
       let elements = document.getElementById('subNavigation').getElementsByClassName("subNavigationButton");
       for (let item of elements) {
         item.classList.remove("active");
       }
 
-      item.classList.add("active");
+      this.classList.add("active");
 
       let cards = document.getElementsByClassName("card")
       for (let card of cards){
         card.style.display = "none";
       }
 
-      console.log(item.getAttribute("searchtag"));
+      console.log(this.getAttribute("searchtag"));
 
-      cards = document.getElementsByClassName(item.getAttribute("searchtag"))
+      cards = document.getElementsByClassName(this.getAttribute("searchtag"))
       for (let card of cards){
         card.style.display = "block";
       }
@@ -31,22 +31,22 @@ elements = document.getElementsByClassName('navButton');
 for (let item of elements) {
   item.addEventListener("click", function() {
     event.preventDefault();
-    if(!item.classList.contains("active")){
+    if(!this.classList.contains("active")){
       let elements = document.getElementsByClassName("navButton");
       for (let item of elements) {
         item.classList.remove("active");
       }
 
-      item.classList.add("active");
+      this.classList.add("active");
 
       let containers = document.getElementsByClassName("content");
       for (let item of containers) {
         item.style.display="none";
       }
 
-      console.log(item.getAttribute("searchid"));
+      console.log(this.getAttribute("searchid"));
 
-      document.getElementById(item.getAttribute("searchid")).style.display="block";
+      document.getElementById(this.getAttribute("searchid")).style.display="block";
     }
   });
 }
@@ -57,10 +57,10 @@ elements = document.getElementsByClassName('topBarButton');
 for (let item of elements) {
   item.addEventListener("click", function() {
     event.preventDefault();
-    if(!item.classList.contains("active")){
+    if(!this.classList.contains("active")){
       event.stopPropagation();
 
-      console.log(item.getAttribute("searchid"));
+      console.log(this.getAttribute("searchid"));
 
       let elements = document.getElementsByClassName("topBarButton");
       for (let item of elements) {
@@ -69,9 +69,9 @@ for (let item of elements) {
         document.getElementById(item.getAttribute("searchid")).style.display = "none";
       }
 
-      item.classList.add("active");
+      this.classList.add("active");
 
-      document.getElementById(item.getAttribute("searchid")).style.display = "block";
+      document.getElementById(this.getAttribute("searchid")).style.display = "block";
 
     }
   });
@@ -103,19 +103,101 @@ for (let item of elements) {
 
   item.addEventListener("click", function() {
     event.preventDefault();
-    if(!item.classList.contains("active")){
+    if(!this.classList.contains("active")){
 
       let elements = document.getElementsByClassName("cardDisplayToggleItem");
       for (let item of elements) {
         item.classList.remove("active");
       }
 
-      item.classList.add("active");
+      this.classList.add("active");
 
-      console.log(item.getAttribute("toggletype"));
+      console.log(this.getAttribute("toggletype"));
 
 
-      document.getElementById("cardHolder").className = item.getAttribute("toggletype");
+      document.getElementById("cardHolder").className = this.getAttribute("toggletype");
     }
   });
 }
+
+//=============
+
+document.getElementById("overlay").addEventListener("click", function() {
+  document.getElementById("overlay").classList.toggle("active");
+
+  let elements = document.getElementsByClassName("overlayCard");
+  for (let item of elements) {
+    item.classList.remove("active");
+  }
+
+});
+
+elements = document.getElementsByClassName("card");
+for (let item of elements) {
+
+  item.getElementsByTagName("img")[0].addEventListener("click", function() {
+
+      document.getElementById("cardInfo").classList.add("active");
+
+    document.getElementById("overlay").classList.toggle("active");
+  });
+}
+
+//=========
+
+elements = document.getElementsByClassName("topBarFSMenuButton");
+
+for (let item of elements) {
+  item.addEventListener("click", function() {
+    event.preventDefault();
+
+      document.getElementById(this.getAttribute("searchid")).classList.add("active");
+
+    document.getElementById("overlay").classList.toggle("active");
+  });
+}
+
+//=======
+
+document.getElementById("tryLogIn").addEventListener("click", function() {
+
+  let password = document.getElementById("passwordInput").value;
+  let username = document.getElementById("usernameInput").value;
+
+  document.getElementById("tryLogIn").disabled = true;
+
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", "http://localhost/api/public/getValidation", true);
+  //xhr.setRequestHeader('X-Auth', '123');
+  xhr.onload = function (e) {
+    document.getElementById("tryLogIn").disabled = false;
+    if (xhr.status === 200) {
+      let resp = JSON.parse(xhr.responseText)
+      console.log(resp);
+      if (resp.isloggedin) {
+        // succsesfully logged in
+        document.getElementById("profileButton").classList.toggle("active");
+        document.getElementById("loginButton").classList.toggle("active");
+        //====
+        document.getElementById("overlay").classList.toggle("active");
+
+        let elements = document.getElementsByClassName("overlayCard");
+        for (let item of elements) {
+          item.classList.remove("active");
+        }
+        //====
+
+      }
+    } else {
+      console.error(xhr.statusText);
+    }
+  };
+
+  if(username == undefined || password == undefined){
+    console.error("no params");
+    document.getElementById("tryLogIn").disabled = false;
+  } else {
+    xhr.send(JSON.stringify({"username":username,"password":password}));
+  }
+
+});
