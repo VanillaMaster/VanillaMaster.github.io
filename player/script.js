51f2d75d1bbfa6a9592b8a0c6c2be5f6cb94d499
Vanilla <36816539+VanillaMaster@users.noreply.github.com>
2021-06-08 22:57:34 +0500

    Add files via upload

--- player/script.js
+++ player/script.js
@@ -6,16 +6,16 @@ const height = parseInt(getComputedStyle(document.documentElement).getPropertyVa
 
 
 playerPane.addEventListener('scroll', function(e) {
-  playerWindow.style.height = `${((pseudoPlayerWindow.offsetHeight - height) * this.scrollTop / (this.scrollHeight - this.offsetHeight) + height).toFixed(0)}px`;
-  console.log((this.scrollTop / (this.scrollHeight - this.offsetHeight)).toFixed(2));
-  if (1 == (this.scrollTop / (this.scrollHeight - this.offsetHeight)).toFixed(2)) {
-    playerPane.classList.remove("active");
-  } else if (0 == (this.scrollTop / (this.scrollHeight - this.offsetHeight)).toFixed(2)) {
-    playerPane.classList.remove("active");
-  }
+  let percent = (this.scrollTop / (this.scrollHeight - this.offsetHeight)).toFixed(2);
+  playerWindow.style.height = `${((pseudoPlayerWindow.offsetHeight - height) * percent + height).toFixed(0)}px`;
+
 });
 
 
 document.getElementById("playerWindow").addEventListener("touchstart", function(e){
   playerPane.classList.add("active");
+});
+document.getElementById("playerWindow").addEventListener("touchend", function(e){
+  playerPane.classList.remove("active");
+  playerPane.scroll({top: playerPane.scrollTop, behavior: 'smooth'});
 });
