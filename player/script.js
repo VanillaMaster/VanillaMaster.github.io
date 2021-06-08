4ff1ba7e936da5a06ac3b0eea28811defef566e7
Vanilla <36816539+VanillaMaster@users.noreply.github.com>
2021-06-08 21:33:31 +0500

    Add files via upload

--- player/script.js
+++ player/script.js
@@ -7,7 +7,7 @@ const height = parseInt(getComputedStyle(document.documentElement).getPropertyVa
 
 playerPane.addEventListener('scroll', function(e) {
   playerWindow.style.height = `${((pseudoPlayerWindow.offsetHeight - height) * this.scrollTop / (this.scrollHeight - this.offsetHeight) + height).toFixed(0)}px`;
-  if ([0,1].includes(this.scrollTop / (this.scrollHeight - this.offsetHeight))) {
+  if ([0,1].includes((this.scrollTop / (this.scrollHeight - this.offsetHeight)).toFixed(2))) {
     playerPane.classList.remove("active");
   }
 });
