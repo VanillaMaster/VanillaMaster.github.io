e80a2eafc5dd99076f12b8afada0bbc61ed942fe
Vanilla <36816539+VanillaMaster@users.noreply.github.com>
2021-06-08 21:24:27 +0500

    Add files via upload

--- player/script.js
+++ player/script.js
@@ -1,10 +1,18 @@
 const playerWindow = document.getElementById("playerWindow");
 const pseudoPlayerWindow = document.getElementById("pseudoPlayerWindow");
+const playerPane = document.getElementById('playerPane');
 
 const height = parseInt(getComputedStyle(document.documentElement).getPropertyValue('--minimized-player-height'));
 
-console.log(height);
 
-document.getElementById('playerPane').addEventListener('scroll', function(e) {
+playerPane.addEventListener('scroll', function(e) {
   playerWindow.style.height = `${((pseudoPlayerWindow.offsetHeight - height) * this.scrollTop / (this.scrollHeight - this.offsetHeight) + height).toFixed(0)}px`;
+  if ([0,1].includes(this.scrollTop / (this.scrollHeight - this.offsetHeight))) {
+    playerPane.classList.remove("active");
+  }
+});
+
+
+document.getElementById("playerWindow").addEventListener("touchstart", function(e){
+  playerPane.classList.add("active");
 });
