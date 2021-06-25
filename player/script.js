f0ee56695d7f56213766280cfc40d7dc745e6237
Vanilla <36816539+VanillaMaster@users.noreply.github.com>
2021-06-26 03:01:04 +0500

    Add files via upload

--- player/script.js
+++ player/script.js
@@ -3,13 +3,57 @@ const pseudoPlayerWindow = document.getElementById("pseudoPlayerWindow");
 const playerContant = document.getElementById('playerContant');
 const dockbar = document.getElementById('dockbar');
 
+const playerContainer = document.getElementById("playerContainer");
+
 const height = parseInt(getComputedStyle(document.documentElement).getPropertyValue('--minimized-player-height'));
 const dockbarHeight = parseInt(getComputedStyle(document.documentElement).getPropertyValue('--bottom-dock-bar-height'));
 
+var isMinified = true;
+var isMaximized = false;
 
-document.getElementsByTagName('body')[0].addEventListener('scroll', function(e) {
+document.getElementById("body").addEventListener('scroll', function(e) {
   let percent = (this.scrollTop / (this.scrollHeight - this.offsetHeight)).toFixed(2);
   playerWindow.style.height = `${((pseudoPlayerWindow.offsetHeight - height) * percent + height).toFixed(0)}px`;
   playerContant.style.opacity = percent;
   dockbar.style.bottom = `${(-percent*dockbarHeight).toFixed(2)}px`
+
+
+  if (isMinified){
+    if(percent != 0){
+      isMinified = false;
+      console.log(`a:${percent}`);
+      playerContainer.classList.add("uncollapsed");
+    }
+  } else {
+    if (percent == 0) {
+      isMinified = true;
+      console.log(`b:${percent}`);
+      playerContainer.classList.remove("uncollapsed");
+    }
+  }
+
+  if (isMaximized){
+    if(percent != 1){
+      isMaximized = false;
+      console.log(`c:${percent}`);
+      playerContainer.classList.add("uncollapsed");
+    }
+  } else {
+    if (percent == 1) {
+      isMaximized = true;
+      console.log(`d:${percent}`);
+      playerContainer.classList.remove("uncollapsed");
+    }
+  }
+
 });
+
+
+playerWindow.addEventListener("touchend",() => {
+  playerContainer.classList.add("untouched")
+});
+playerWindow.addEventListener("touchstart",() => {
+  playerContainer.classList.remove("untouched")
+});
+
+document.body.ontouchstart = (e) => {console.log(e.target);}
