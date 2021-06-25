dcc1ee192e8519712d7bbd5ee77d7e3bf6ab2bb1
Vanilla <36816539+VanillaMaster@users.noreply.github.com>
2021-06-26 03:11:53 +0500

    Add files via upload

--- player/script.js
+++ player/script.js
@@ -12,20 +12,20 @@ var isMinified = true;
 var isMaximized = false;
 
 document.getElementById("body").addEventListener('scroll', function(e) {
-  let percent = (this.scrollTop / (this.scrollHeight - this.offsetHeight)).toFixed(4);
+  let percent = parseFloat((this.scrollTop / (this.scrollHeight - this.offsetHeight)).toFixed(4));
   playerWindow.style.height = `${((pseudoPlayerWindow.offsetHeight - height) * percent + height).toFixed(2)}px`;
   playerContant.style.opacity = percent;
   dockbar.style.bottom = `${(-percent*dockbarHeight).toFixed(2)}px`
 
 
   if (isMinified){
-    if(percent != 0){
+    if(percent.toFixed(1) != 0){
       isMinified = false;
       console.log(`a:${percent}`);
       playerContainer.classList.add("uncollapsed");
     }
   } else {
-    if (percent == 0) {
+    if (percent.toFixed(1) == 0) {
       isMinified = true;
       console.log(`b:${percent}`);
       playerContainer.classList.remove("uncollapsed");
@@ -33,13 +33,13 @@ document.getElementById("body").addEventListener('scroll', function(e) {
   }
 
   if (isMaximized){
-    if(percent != 1){
+    if(percent.toFixed(1) != 1){
       isMaximized = false;
       console.log(`c:${percent}`);
       playerContainer.classList.add("uncollapsed");
     }
   } else {
-    if (percent == 1) {
+    if (percent.toFixed(1) == 1) {
       isMaximized = true;
       console.log(`d:${percent}`);
       playerContainer.classList.remove("uncollapsed");
