97b1dca079fda95b38646d611e5d7093a47a02ad
Vanilla <36816539+VanillaMaster@users.noreply.github.com>
2021-11-18 19:21:48 +0300

    Add files via upload

--- alg_and_data_structs/lab5/script.js
+++ alg_and_data_structs/lab5/script.js
@@ -1,55 +1,8 @@
-
 const processors = {
   1:pushRelable,
 }
 
-console.log(`0 10 13 0 0 0
-0 0 3 0 0 0
-0 0 0 6 0 7
-0 0 0 0 10 0
-0 0 0 0 0 5
-0 0 0 0 0 0`);
-
-const input = document.getElementById('data_input');
-input.addEventListener("input",(e)=>{
-  input.style.width = "";
-  input.style.height = "";
-
-  input.style.width = `${input.scrollWidth}px`;
-  input.style.height = `${input.scrollHeight}px`;
-});
-
-const button = document.getElementById('button');
-const out = document.getElementById("output");
-const select = document.getElementById("select");
-const sinkInput = document.getElementById("sinkInput");
-const sourceInput = document.getElementById("sourceInput");
-button.addEventListener("click",(e)=>{
-  let matrix = parseMatrix(input.value);
-  displayMatrix(matrix);
-
-  let data = processors[select.value](matrix,parseInt(sourceInput.value),parseInt(sinkInput.value));
-  out.innerText = data;
-});
-
-function parseMatrix(data) {
-  let matrix = data.split("\n");
-
-  for (let i = 0; i < matrix.length; i++) {
-    matrix[i] = matrix[i].split(" ");
-    for (let j = 0; j < matrix[i].length; j++) {
-      matrix[i][j] = parseInt(matrix[i][j]);
-    }
-  }
-
-  return matrix
-}
-
-const visualize = document.body.querySelector(".visualize");
-function displayMatrix(matrix) {
-  matrix = matrix.join("\n").replaceAll(","," ");
-  visualize.src = `https://graphonline.ru/?matrix=${encodeURI(matrix)}&separator=space`;
-}
+export {processors};
 
 function getAdjacency(matrix) {
   let adjacency = [];
