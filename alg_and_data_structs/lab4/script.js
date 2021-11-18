f07e43a842bfe4a87dbf0ca3b984a28bd8789d57
Vanilla <36816539+VanillaMaster@users.noreply.github.com>
2021-11-18 19:16:40 +0300

    Add files via upload

--- alg_and_data_structs/lab4/script.js
+++ alg_and_data_structs/lab4/script.js
@@ -1,52 +1,8 @@
-
 const processors = {
-  //1:getArtPoints,
-  //2:getBridges,
+  1:khun,
 }
 
-const input = document.getElementById('data_input');
-input.addEventListener("input",(e)=>{
-  input.style.width = "";
-  input.style.height = "";
-
-  input.style.width = `${input.scrollWidth}px`;
-  input.style.height = `${input.scrollHeight}px`;
-});
-
-const button = document.getElementById('button');
-const out = document.getElementById("output");
-const select = document.getElementById("select");
-button.addEventListener("click",(e)=>{
-  let matrix = parseMatrix(input.value);
-  displayMatrix(matrix);
-
-  //console.log(checkBipart(matrix));
-
-  console.log(blossom(matrix));
-
-  //let data = getDiameterWidth(matrix);
-  //let data = processors[select.value](matrix);
-  //out.innerText = data;
-});
-
-function parseMatrix(data) {
-  let matrix = data.split("\n");
-
-  for (let i = 0; i < matrix.length; i++) {
-    matrix[i] = matrix[i].split(" ");
-    for (let j = 0; j < matrix[i].length; j++) {
-      matrix[i][j] = parseInt(matrix[i][j]);
-    }
-  }
-
-  return matrix
-}
-
-const visualize = document.body.querySelector(".visualize");
-function displayMatrix(matrix) {
-  matrix = matrix.join("\n").replaceAll(","," ");
-  visualize.src = `https://graphonline.ru/?matrix=${encodeURI(matrix)}&separator=space`;
-}
+export {processors};
 
 function getAdjacency(matrix) {
   let adjacency = [];
@@ -71,7 +27,6 @@ function checkBipart(matrix) {
   for (let i = 0; i < visited.length; i++) {
     let item = visited[i]
     if (isNaN(item)) {
-      //console.log(`start: ${item}`);
       if (dfs([i])) {
         return false;
       }
@@ -87,9 +42,6 @@ function checkBipart(matrix) {
       visited[curent] = true;
     }
     let neighbors = adjacency[curent];
-
-    //console.log(`path: ${path.toString()}`);
-    //console.log(`visited: ${visited.toString()}`);
 
     let unVisitedNeighbors = [];
     for (let i = 0; i < neighbors.length; i++) {
@@ -112,11 +64,10 @@ function checkBipart(matrix) {
     } else {
       return false;
     }
-
   }
 }
 
-function blossom(matrix) {
+function khun(matrix) {
 
   let adjacency = getAdjacency(matrix);
 
@@ -126,19 +77,20 @@ function blossom(matrix) {
   while (check) {
     check = improveMatching(G);
   }
-  console.log("end");
 
-  console.log(G);
+  let result = "";
+  G.matching.forEach((edge, i) => {
+    result+= `(${edge.start}:${edge.end});`
+  });
+
+  return result;
 
   function improveMatching(graph) {
     let exposed = getExposed(graph);
     for (let i = 0; i < exposed.length; i++) {
       let path = findAugPath(graph,exposed[i]);
       if (path) {
-        //switch here
-
         path.forEach((item, i) => {
-
           for (let j = (graph.matching.length - 1); j >= 0; j--) {
             let edge = graph.matching[j];
             if (edge.start == item || edge.end == item) {
@@ -147,27 +99,13 @@ function blossom(matrix) {
               graph.matching.splice(j,1);
             }
           }
-          /*
-          graph.matching.forEach((edge, j) => {
-            if (edge.x == item || edge.y == item) {
-              graph.nodes[edge.x].isExposed = true;
-              graph.nodes[edge.y].isExposed = true;
-              graph.matching.splice(j,1);
-            }
-          });
-          */
-          //graph.matching.splice(graph.matching.indexOf(edge),1);
         });
 
-
         for (let j = 0; j < path.length; j+=2) {
-          console.log(`${path[j]}:${path[j+1]}`);
           graph.matching.push({start:path[j],end:path[j+1]});
-
           graph.nodes[(path[j])].isExposed = false;
           graph.nodes[(path[j+1])].isExposed = false;
         }
-        console.log(path);
         return true;
       }
     }
@@ -184,7 +122,6 @@ function blossom(matrix) {
   }
 
   function findAugPath(graph,node) {
-
     let parents = new Array(graph.nodes.length).fill(NaN);
     parents[node.index] = null;
     let qeue = [node];
@@ -217,12 +154,10 @@ function blossom(matrix) {
                 break;
               }
             }
-
           }
         }
       }
     }
     return null;
   }
-
 }
