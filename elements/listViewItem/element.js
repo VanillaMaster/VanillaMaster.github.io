f5b1e9263875d05edebadfc4b58ad5b31b483c32
Vanilla <36816539+VanillaMaster@users.noreply.github.com>
2022-06-30 20:30:33 +0300

    Add files via upload

---
+++ elements/listViewItem/element.js
@@ -0,0 +1,51 @@
+class ListViewItem extends HTMLElement {
+    constructor() {
+        super();
+        this.load();
+    }
+    load(){
+        const fragment = ListViewItem.template.cloneNode(true)
+
+        let iconHolder = fragment.querySelector(".left");
+        iconHolder.addEventListener("pointerleave",(e)=>{
+            console.log(1);
+        })
+
+        this.#shadow = this.attachShadow({mode:'open'});
+        this.#shadow.append(fragment);
+    }
+    #shadow;
+
+    connectedCallback(){
+        this.#isSelected = this.hasAttribute("selected")
+        this.addEventListener("contextmenu",(e)=>{
+            //console.log(e.isTrusted);
+            if (e.isTrusted === false) {
+                if (this.#isSelected){
+                    this.removeAttribute("selected");
+                    this.#isSelected = false;
+                } else {
+                    this.setAttribute("selected","");
+                    this.#isSelected = true;
+                }
+            }
+        })
+    }
+    #isSelected = false;
+
+}
+
+(async function () {
+    let html = await ((await fetch("/elements/ListViewItem/element.html")).text());
+    return document.createRange().createContextualFragment(html);
+})().then((template)=>{
+
+    Object.defineProperty(ListViewItem,"template",{
+        value: template,
+        enumerable: false,
+        configurable: false,
+        writable: false,
+    });
+
+    customElements.define("list-view-item",ListViewItem);
+});
